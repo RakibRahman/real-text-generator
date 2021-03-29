aa7a9100a41cbdb804fc2294872fd6abe4bbdf76
RakibRahman <md.rr.talukder@gmail.com>
2021-03-29 14:41:03 +0600

    Generator added

--- app.js
+++ app.js
@@ -11,8 +11,26 @@ const array = text.split(" ");
 //console.log(array);
 
 btn.addEventListener("click", () => {
-  arr.sort((a, b) => 0.5 - Math.random());
-  console.log(arr);
+  const getInfo = array.sort((a, b) => 0.5 - Math.random());
+  const paraLength = Number(paraNumbers.value);
+  const wordsLength = Number(wordNumbers.value);
+
+  for (let i = 0; i < paraLength; i++) {
+    generateParas(wordsLength);
+  }
 });
 
-const generateParas = () => {};
+const generateParas = (num) => {
+  const p = document.createElement("p");
+  let temp = "";
+  let randomWords = Math.floor(Math.random() * 10) + 2;
+
+  for (let i = 0; i < randomWords; i++) {
+    let words = array[Math.floor(Math.random() * array.length)];
+    temp += ` ${words}`;
+  }
+  temp = temp.trim().toLowerCase();
+  p.textContent = temp;
+  p.style.margin = "10px 0px";
+  output.append(p);
+};
